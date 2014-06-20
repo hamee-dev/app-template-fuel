fa8fae31ae3fdaae213852da749c5dd0bed826dc
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-20 18:49:21 +0900

    companiesテーブルを生成するマイグレーションを作成

---
+++ fuel/app/migrations/001_create_companies.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_companies
+{
+	public function up()
+	{
+		\DBUtil::create_table('companies', array(
+			'id' 				=> array('constraint' => 11,  'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'main_function_id' 	=> array('constraint' => 128, 'type' => 'char'),
+			'platform_id' 		=> array('constraint' => 128, 'type' => 'char'),
+			'updated_at' 		=> array('type' => 'timestamp'),
+			'created_at' 		=> array('type' => 'timestamp'),
+
+		), array('id'), true, 'InnoDB');
+
+		\DBUtil::create_index('companies', 'main_function_id', 'unique_main_function_id', 'unique');
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_index('companies', 'unique_main_function_id');
+		\DBUtil::drop_table('companies');
+	}
+}
