57d71a4797b34ba46cbd06ed4757099f32861810
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-23 09:17:45 +0900

    \Base\Apiまでを名前空間とし、Clientクラスを配置、api.phpは削除

--- fuel/packages/nextengine/bootstrap.php
+++ fuel/packages/nextengine/bootstrap.php
@@ -3,7 +3,7 @@
 Autoloader::add_core_namespace('Nextengine');
 
 Autoloader::add_classes(array(
-	'Nextengine\\NextengineApiException' => __DIR__ . '/classes/nextengine/api.php',
-	'Nextengine\\Nextengine_Api' => __DIR__ . '/classes/nextengine/api.php',
+	// 'Nextengine\\Api\\NextengineApiException' => __DIR__ . '/classes/nextengine/api.php',
+	'Nextengine\\Api\\Client' => __DIR__ . '/classes/nextengine/api/client.php',
 
 ));
--- fuel/packages/nextengine/classes/nextengine/api/client.php
+++ fuel/packages/nextengine/classes/nextengine/api/client.php
@@ -1,8 +1,87 @@
 <?php
 
-namespace Nextengine;
+namespace Nextengine\Api;
 
-class Nextengine_Api_Client extends Nextengine_Api
+require_once __DIR__.DS.'..'.DS.'..'.DS.'neApiClient.php';
+
+// class NextengineApiException extends \FuelException {}
+
+class Client extends \neApiClient
 {
-	
+	/**
+	 * Default config
+	 * @var array
+	 */
+	protected static $_defaults = array();
+
+	/**
+	* Driver config
+	* @var array
+	*/
+	protected $config = array();
+
+	/**
+	 * Init
+	 */
+	public static function _init()
+	{
+		self::$_defaults = \Config::load('nextengine', true);
+	}
+
+	/**
+	 * Nextengine driver forge.
+	 *
+	 * @param	array			$config		Config array
+	 * @return  Nextengine
+	 */
+	public static function forge($config = array())
+	{
+		$config = \Arr::merge(static::$_defaults, \Config::get('nextengine', array()), $config);
+
+		$class = new static($config);
+
+		return $class;
+	}
+
+	/**
+	* Driver constructor
+	*
+	* @param array $config driver config
+	*/
+	public function __construct(array $config = array())
+	{
+		$config = \Arr::merge($config, self::$_defaults);
+
+		$this->_client_id		= $config['client_id'];
+		$this->_client_secret	= $config['client_secret'];
+		$this->_redirect_uri	= $config['redirect_uri'];
+
+		parent::__construct($this->_client_id, $this->_client_secret, $this->_redirect_uri);
+	}
+
+	/**
+	* Get a config setting.
+	*
+	* @param string $key the config key
+	* @param mixed  $default the default value
+	* @return mixed the config setting value
+	*/
+	public function get_config($key, $default = null)
+	{
+		return \Arr::get($this->config, $key, $default);
+	}
+
+	/**
+	* Set a config setting.
+	*
+	* @param string $key the config key
+	* @param mixed $value the new config value
+	* @return object $this for chaining
+	*/
+	public function set_config($key, $value)
+	{
+		\Arr::set($this->config, $key, $value);
+
+		return $this;
+	}
 }
