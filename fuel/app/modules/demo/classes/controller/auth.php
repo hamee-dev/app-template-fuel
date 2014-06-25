c2abdb44e920104f87e9c21c35a297a1fe302408
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-25 17:13:05 +0900

    callbackでの処理を簡素化

--- fuel/app/modules/demo/classes/controller/auth.php
+++ fuel/app/modules/demo/classes/controller/auth.php
@@ -35,45 +35,44 @@ class Controller_Auth extends \Controller {
 	 */
 	public function action_callback()
 	{
-		// uidとstateがURLで渡っていたら
-		if(\Input::get('uid') && \Input::get('state')) {
-			// 1-1. アクセストークン・リフレッシュトークンを取得
-			$auth_response = self::$client->apiExecute('/api_neauth', array(
-				'uid' => \Input::get('uid'),
-				'state' => \Input::get('state'),
-			));
-			// 1-2. 取得したアクセストークンを利用して企業情報を取得
-			$company_info = self::$client->apiExecute('/api_v1_login_company/info');
-			$company_info = $company_info['data'][0];
-
-
-			// 2-1. ログインされた企業がDBに存在しなければ、企業情報をDBに格納
-			$company = new \Model_Company();
-
-			$company->platform_id      = $auth_response['company_ne_id'];
-			$company->main_function_id = $company_info['company_id'];
-			$company->save();
-
-			// 2-2. ログインされたユーザがDBに存在しなければ、ユーザ情報をDBに格納
-			$user = new \Model_User();
-
-			$user->company_id     = $company->id;
-			$user->uid            = $auth_response['uid'];
-			$user->next_engine_id = $auth_response['pic_ne_id'];
-			$user->email          = $auth_response['pic_mail_address'];
-			$user->access_token   = $auth_response['access_token'];
-			$user->refresh_token  = $auth_response['refresh_token'];
-			$user->created_at     = \DB::expr('NOW()');
-			$user->save();
-
-			// 3-1. セッションにログインユーザの情報をセット
-			\Session::set('account.company', $company);
-			\Session::set('account.user', $user);
-
-		// uidとstateがURLになく、セッションにも認証情報がない場合、再認証する
-		} else if(is_null(\Session::get('account.company')) && is_null(\Session::get('account.user'))) {
-			\Response::redirect(\Uri::create('/demo/auth/login'));
+		if(!is_null(\Session::get('account.company')) && !is_null(\Session::get('account.user'))) {
+			// TODO: 目的の画面へリダイレクト
 		}
+
+		// なぜ２個API叩くのか補足
+		// 1-1. アクセストークン・リフレッシュトークンを取得
+		$user_info = self::$client->apiExecute('/api_v1_login_user/info');
+		$user_info = $user_info['data'][0];
+
+		// 1-2. 取得したアクセストークンを利用して企業情報を取得
+		$company_info = self::$client->apiExecute('/api_v1_login_company/info');
+		$company_info = $company_info['data'][0];
+
+		// var_dump($company_info);
+		// exit();
+
+		// 2-1. ログインされた企業がDBに存在しなければ、企業情報をDBに格納
+		$company = new \Model_Company();
+
+		$company->platform_id      = $company_info['company_ne_id'];
+		$company->main_function_id = $company_info['company_id'];
+		$company->save(true);
+
+		// 2-2. ログインされたユーザがDBに存在しなければ、ユーザ情報をDBに格納
+		$user = new \Model_User();
+
+		$user->company_id     = $company->id;
+		$user->uid            = $user_info['uid'];
+		$user->next_engine_id = $user_info['pic_ne_id'];
+		$user->email          = $user_info['pic_mail_address'];
+		$user->access_token   = self::$client->_access_token;
+		$user->refresh_token  = self::$client->_refresh_token;
+		$user->created_at     = \DB::expr('NOW()');
+		$user->save(true);
+
+		// 3-1. セッションにログインユーザの情報をセット
+		\Session::set('account.company', $company);
+		\Session::set('account.user', $user);
 
 		echo "<a href='".\Uri::create('/demo/api/find')."'>APIのデモを見る</a>";
 	}
