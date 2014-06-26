400df031b9a74a1291ace76bfdb8a15f8ac8aa30
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-26 16:21:43 +0900

    Nextengine\Api\Clientの継承クラスのクラス名が間違っていたので修正

--- fuel/packages/nextengine/bootstrap.php
+++ fuel/packages/nextengine/bootstrap.php
@@ -5,7 +5,7 @@ Autoloader::add_core_namespace('Nextengine');
 Autoloader::add_classes(array(
 	'Nextengine\\Api\\NextengineApiException' => __DIR__ . '/classes/nextengine/api/client.php',
 	'Nextengine\\Api\\Client' => __DIR__ . '/classes/nextengine/api/client.php',
-	'Nextengine\\Api\\Router' => __DIR__ . '/classes/nextengine/api/client/router.php',
-	'Nextengine\\Api\\Batch' => __DIR__ . '/classes/nextengine/api/client/batch.php',
+	'Nextengine\\Api\\Client_Router' => __DIR__ . '/classes/nextengine/api/client/router.php',
+	'Nextengine\\Api\\Client_Batch' => __DIR__ . '/classes/nextengine/api/client/batch.php',
 
 ));
