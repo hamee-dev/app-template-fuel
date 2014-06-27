f2cfe5b04ac02eab92794a6fde907524f860d08f
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-27 15:30:53 +0900

    設定のredirect_urlから/demoを除去

--- fuel/packages/nextengine/config/nextengine.php
+++ fuel/packages/nextengine/config/nextengine.php
@@ -3,5 +3,5 @@
 return array(
 	'client_id'		=> '2zG7d5MjXPh4m8',
 	'client_secret' => 'FTNubmlpyAgE5e3BnqWt6IHJ18voxVkMS9Yh4Zjc',
-	'redirect_uri'	=> 'https://localhost:8443/ne-base/demo/auth/callback'
+	'redirect_uri'	=> 'https://localhost:8443/ne-base/auth/callback'
 );
