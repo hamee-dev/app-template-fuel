b85224d27ef5288b4c1e2aeaef3f84afb91022e2
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-27 13:49:01 +0900

    保持するように変更し、アクセストークンに変更があったら更新

--- fuel/packages/nextengine/classes/nextengine/api/client.php
+++ fuel/packages/nextengine/classes/nextengine/api/client.php
@@ -24,6 +24,12 @@ class Client extends \neApiClient
 	protected static $_defaults = array();
 
 	/**
+	 * ユーザオブジェクトを格納する
+	 * @var array
+	 */
+	protected $user = null;
+
+	/**
 	 * 設定をロードし格納しておく
 	 */
 	public static function _init()
@@ -34,11 +40,10 @@ class Client extends \neApiClient
 	/**
 	 * FuelPHP式（ファクトリメソッド）のコンストラクタ
 	 *
-	 * @param  Model_User $user   ユーザのインスタンス
 	 * @param  array      $config 設定値（この値が優先される）
 	 * @return Nextengine\Api\Client
 	 */
-	public static function forge(\Model_User $user, $config = array())
+	public static function forge(array $config = array())
 	{
 		$config = \Arr::merge(static::$_defaults, $config);
 
@@ -49,21 +54,39 @@ class Client extends \neApiClient
 
 	/**
 	 * コンストラクタに与えられた設定でデフォルト設定を上書きし、接続に必要な情報を格納する
-	 * @param  Model_User $user   ユーザのインスタンス
 	 * @param  array      $config 設定値（この値が優先される）
 	 */
-	public function __construct(\Model_User $user, array $config = array())
+	public function __construct(array $config = array())
 	{
 		$config = \Arr::merge(self::$_defaults, $config);
-
-		$this->user = $user;
 
 		parent::__construct(
 			$config['client_id'],
 			$config['client_secret'],
-			$config['redirect_uri'],
-			$user->access_token,
-			$user->refresh_token);
+			$config['redirect_uri']);
+	}
+
+	/**
+	 * ユーザ情報をセットする
+	 * @param Model_User $user ユーザのインスタンス
+	 * @return void
+	 */
+	public function setUser($user) {
+		// クライアントのアクセストークンがNULL以外で、DBの値と違っていたら、DBを更新
+		// NOTE: クライアントのプロパティが最も最新、DBの値はその次に新しい。優先すべきはクライアントのプロパティ。
+		if(!is_null($this->_access_token) && $user->access_token !== $this->_access_token) {
+			$user->access_token = $this->_access_token;
+			$user->refresh_token = $this->_refresh_token;
+			$user->save();
+		}
+
+		// まだAPIを叩いていない状態で、アクセストークンがあるユーザが渡されたら、クライアントのプロパティを更新
+		if(!is_null($user->access_token)) {
+			$this->_access_token = $user->access_token;
+			$this->_refresh_token = $user->refresh_token;
+		}
+
+		$this->user = $user;
 	}
 
 	/**
@@ -76,35 +99,26 @@ class Client extends \neApiClient
 	 * @override
 	 */
 	public function apiExecute($path, $api_params = array(), $redirect_uri = NULL) {
-		$current_access_token = $this->_access_token;
-
+		$before_exec_access_token = $this->_access_token;
 		$response = parent::apiExecute($path, $api_params, $redirect_uri);
 
-		// TODO: resultがsuccessじゃなかったらエラーコードによって例外を投げる
+		// TODO: resultがsuccessじゃなかったら、エラーコードによって例外を投げる
+		// エラーの種類については→を参照：http://api.next-e.jp/message.php
+		// エラー時の振る舞いについては、継承クラスのfailoverメソッドを参照。
 		if($response['result'] !== self::RESULT_SUCCESS) {
 			$this->failover($response['code'], $response['message']);
 		}
 
-		if($current_access_token !== $this->_access_token) {
+		if(!is_null($this->user) && $before_exec_access_token !== $this->_access_token) {
 			$this->user->access_token  = $this->_access_token;
 			$this->user->refresh_token = $this->_refresh_token;
 			$this->user->save();
+
+			\Session::set('account.user', $this->user);
 		}
 
 		return $response;
 	}
-
-	/**
-	 * ネクストエンジンAPIにログインを行う
-	 * 
-	 * ### 拡張点
-	 * 今のところ無いのでコメントアウト
-	 * 
-	 * @override
-	 */
-	// public function neLogin($redirect_uri = NULL) {
-	// 	return parent::neLogin($redirect_uri);
-	// }
 
 	/**
 	 * ネクストエンジンAPIからエラーが帰っていた場合の処理を行う
--- fuel/packages/nextengine/classes/nextengine/api/client/router.php
+++ fuel/packages/nextengine/classes/nextengine/api/client/router.php
@@ -5,6 +5,42 @@ namespace Nextengine\Api;
 class Client_Router extends Client
 {
 	/**
+	 * ユーザのuidを用いて認証を行う。
+	 * DB < APIの順でコストが高いので、まずDBを見てから、仕方ない場合のみAPIを叩く形にしている。
+	 * FIXME: 戻り値で配列を使用しているのでややオレオレ仕様になっている。
+	 * 
+	 * ```php
+	 * list($company, $user) = $client->authenticate($uid);
+	 * ```
+	 * 
+	 * @param  string $uid ユーザのuid
+	 * @return array [Model_Company, Model_User]のインスタンスの配列。listで受け取る想定。
+	 */
+	public function authenticate($uid) {
+		// APIを１回は呼ばないといけない
+		$company = $this->_createCompany();
+		$users = \Model_User::findBy('company_id', $company->id);
+
+		// uidに対応するユーザがいる場合、そのまま利用する
+		if(count($users) > 0) {
+			$user    = $users[0];
+
+		// uidに対応するユーザが居ない場合、APIを叩き認証を行う
+		} else {
+			// NOTE: fキー制約の都合でcompanyが先、elseと並び順が違うのは意図的。
+			$user    = $this->_createUser($company->id);
+		}
+
+		$this->setUser($user);
+
+		// apiを1度叩かないと最新のアクセストークンが取得できないので最低１回は叩かないといけない
+		if(count($users) > 0) {
+			$this->apiExecute('/api_v1_login_user/info');
+		}
+		return array($company, $user);
+	}
+
+	/**
 	 * 例外の振り分けを行う
 	 */
 	protected function failover($code, $message) {
@@ -42,5 +78,75 @@ class Client_Router extends Client
 					throw $e;
 			}
 		}
+	}
+
+	// APIから企業情報を取得し、
+	// INSERT IGNORE
+	/**
+	 * APIから情報を取得し企業データをDBに挿入する
+	 * 既にDBに情報が存在している場合はUPDATEがかかる。
+	 * FIXME: Model_BaseでINSERTorUPDATEを未実装のため、
+	 *        INSERT or UPDATEではなく、INSERT IGNOREとSELECT + UPDATEを利用している。とても冗長。
+	 * @return Model_Company 挿入(orDBから取得)したインスタンス
+	 */
+	private function _createCompany() {
+		$company_info = parent::apiExecute('/api_v1_login_company/info');
+		$company_info = $company_info['data'][0];
+
+		// NOTE: http://ne0.next-engine.org:10080/ld3sl/issues/6329
+		//       SELECTを打ってからINSERTをするよりも、INSERT IGNOREを使用したほうが早い
+		//       毎回必ずSELECTが走るよりも、挿入に失敗したらSELECTが走る方がコストが安くなると判断したため
+		//       まず一度挿入を試みて、失敗したら復帰処理としてUPDATEをかけている。
+		$company = new \Model_Company();
+		$company->platform_id      = $company_info['company_ne_id'];
+		$company->main_function_id = $company_info['company_id'];
+
+		// NOTE: 企業データが既に挿入済みのために挿入が発生しなくても、
+		//       $companyのidがないと、fキー制約で$userが保存できなくなるので、無理やり復帰させる
+		if(!$company->save(true)) {
+			$company = \Model_Company::findBy('main_function_id', $company->main_function_id);
+			$company = $company[0];
+		}
+
+		return $company;
+	}
+
+	/**
+	 * APIから情報を取得しユーザデータをDBに挿入する
+	 * 既にDBに情報が存在している場合はUPDATEがかかる。
+	 * FIXME: Model_BaseでINSERTorUPDATEを未実装のため、
+	 *        INSERT or UPDATEではなく、INSERT IGNOREとSELECT + UPDATEを利用している。とても冗長。
+	 * @param  int $company_id
+	 * @return Model_User 挿入(orDBから取得)したインスタンス
+	 */
+	private function _createUser($company_id) {
+		$user_info = parent::apiExecute('/api_v1_login_user/info');
+		$user_info = $user_info['data'][0];
+
+		// NOTE: http://ne0.next-engine.org:10080/ld3sl/issues/6329
+		//       SELECTを打ってからINSERTをするよりも、INSERT IGNOREを使用したほうが早い
+		//       毎回必ずSELECTが走るよりも、挿入に失敗したらSELECTが走る方がコストが安くなると判断したため
+		//       まず一度挿入を試みて、失敗したら復帰処理としてUPDATEをかけている。
+		$user = new \Model_User();
+		$user->company_id     = $company_id;
+		$user->uid            = $user_info['uid'];
+		$user->next_engine_id = $user_info['pic_ne_id'];
+		$user->email          = $user_info['pic_mail_address'];
+		$user->access_token   = $this->_access_token;
+		$user->refresh_token  = $this->_refresh_token;
+		$user->created_at     = \DB::expr('NOW()');
+
+		// NOTE: ユーザデータが既に挿入済みのために挿入が発生しなくても、
+		//       $userとDBの値が同じになっていないと、APIを叩く->ユーザモデルを更新の方式が取れないので、無理やり復帰させる
+		if(!$user->save(true)) {
+			$users = \Model_User::findBy('uid', $user_info['uid']);
+			$user = $users[0];
+
+			$user->access_token   = $this->_access_token;
+			$user->refresh_token  = $this->_refresh_token;
+			$user->save();
+		}
+
+		return $user;
 	}
 }
