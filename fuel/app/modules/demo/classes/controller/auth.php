5b2f1533aa1597894ce41c6fe64e533de19e63ce
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-27 13:49:20 +0900

    認証処理をリファクタ

--- fuel/app/modules/demo/classes/controller/auth.php
+++ fuel/app/modules/demo/classes/controller/auth.php
@@ -10,12 +10,8 @@ class Controller_Auth extends \Controller {
 
 	public static function _init()
 	{
-		$user = \Session::get('account.user');
-		if(is_null($user)) {
-			$user = new \Model_User();
-		}
-
-		self::$client = new \Nextengine\Api\Client($user);
+		// NOTE: 認証画面ではコンストラクタに何も渡せない（login時には何も渡せるものがないので、ナシで統一）
+		self::$client = new \Nextengine\Api\Client_Router();
 	}
 
 	/**
@@ -40,46 +36,27 @@ class Controller_Auth extends \Controller {
 	 */
 	public function action_callback()
 	{
-		if(!is_null(\Session::get('account.company')) && !is_null(\Session::get('account.user'))) {
-			// TODO: 目的の画面へリダイレクト
+		// NOTE: 可読性と関数呼び出しのオーバーヘッド軽減のため、結果を変数にキャッシュ
+		$session_user = \Session::get('account.user');
+		$get_uid      = \Input::get('uid');
+		$get_state    = \Input::get('state');
+
+		// セッションもURLにも何もない = 通常操作では起こりえない非正規ルートなので再認証させる
+		if(is_null($get_uid) && is_null($get_state)) {
+			\Response::redirect('/demo/auth/login');
 		}
 
-		// なぜ２個API叩くのか補足
-		// 1-1. アクセストークン・リフレッシュトークンを取得
-		$user_info = self::$client->apiExecute('/api_v1_login_user/info');
-		$user_info = $user_info['data'][0];
-
-		// 1-2. 取得したアクセストークンを利用して企業情報を取得
-		$company_info = self::$client->apiExecute('/api_v1_login_company/info');
-		$company_info = $company_info['data'][0];
-
-		// 2-1. ログインされた企業がDBに存在しなければ、企業情報をDBに格納
-		$company = new \Model_Company();
-
-		$company->platform_id      = $company_info['company_ne_id'];
-		$company->main_function_id = $company_info['company_id'];
-		$company->save(true);
-
-		// NOTE: 企業データが既に挿入済みで挿入に失敗しても、
-		//       $companyのidがないとfキー制約で$userが保存できなくなるので無理やり復帰させる
-		if(is_null($company->id)) {
-			$company = \Model_Company::findBy('main_function_id', $company->main_function_id);
-			$company = $company[0];
+		// セッションがある = 既にログイン済みなのでセッションのuidを使って認証
+		// URLに何がついてようといなかろうと、セッションの値を使っている。
+		if(!is_null($session_user)) {
+			list($company, $user) = self::$client->authenticate($session_user->uid);
+		} else {
+			// セッションがなくURLにuidとstateが渡っていたら、URLのuidを使って認証
+			// NOTE: GETがない場合は弾いているのである前提でOK
+			list($company, $user) = self::$client->authenticate($get_uid);
 		}
 
-		// 2-2. ログインされたユーザがDBに存在しなければ、ユーザ情報をDBに格納
-		$user = new \Model_User();
-
-		$user->company_id     = $company->id;
-		$user->uid            = $user_info['uid'];
-		$user->next_engine_id = $user_info['pic_ne_id'];
-		$user->email          = $user_info['pic_mail_address'];
-		$user->access_token   = self::$client->_access_token;
-		$user->refresh_token  = self::$client->_refresh_token;
-		$user->created_at     = \DB::expr('NOW()');
-		$user->save(true);
-
-		// 3-1. セッションにログインユーザの情報をセット
+		// セッションにログインユーザの情報をセット
 		\Session::set('account.company', $company);
 		\Session::set('account.user', $user);
 
