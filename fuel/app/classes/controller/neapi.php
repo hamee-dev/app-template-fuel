24b8fd77dceea8c2290d449188f5e1ca30c8f291
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-29 23:57:21 +0900

    Controller_Neapiのテストを追加、ソースのコメントを追加

--- fuel/app/classes/controller/neapi.php
+++ fuel/app/classes/controller/neapi.php
@@ -1,14 +1,29 @@
 <?php
 
-class Controller_Neapi extends Controller_Base {
+/**
+ * ネクストエンジンAPIを利用する際に継承する抽象クラス
+ * APIを使用する画面で共通処理として必要なセッション処理、クライアントの初期化を行う。
+ * 
+ * NOTE: 継承クラスで別途_initの処理を書きたくなった際には、「必ず」parent::_initをコールして下さい。
+ *       こいつを呼んでもらえないと言語ファイルのロードが出来ません。
+ */
+abstract class Controller_Neapi extends Controller_Base {
+	/**
+	 * ネクストエンジンAPIクライアントのインスタンスを格納する
+	 * @var \Nextengine\Api\Client
+	 */
 	protected static $client;
 
+	/**
+	 * APIを使用する画面で共通処理として必要なセッション処理、クライアントの初期化を行う。
+	 * @return void
+	 */
 	public static function _init()
 	{
 		parent::_init();
 		$session_user = \Session::get('account.user');
 
-		// セッション切れ
+		// セッション切れならログイン処理へリダイレクト
 		if(is_null($session_user)) {
 			Response::redirect('/auth/login');
 		}
--- fuel/app/tests/controller/neapi.php
+++ fuel/app/tests/controller/neapi.php
@@ -3,7 +3,25 @@
 /**
  * Controller_Neapiのテスト
  */
-class Test_Controller_Neapi extends PHPUnit_Framework_TestCase
+class Test_Controller_Neapi extends Test_Controller_Base
 {
+	function setUp() {
+		parent::setUp();
 
+		// CLIからだとセッションが何も設定されてないので明示的にセット
+		// FIXME: ハードコードではなく、ユーザ情報のシードをテスト時に投入してそれを利用したい
+		$user = (object)array(
+			'id'  => 20,
+			'uid' => '71c1cc54f91641828805369f1948d22d8389cfefc6ca2661662aa4b5c8f15a61132dddd97710433b13ed9be9656630f040a5e9782f4847471031ecc02cb9d628'
+		);
+		Session::set('account.user', $user);
+	}
+
+	function test_init_を呼ぶとclientにはクライアントのインスタンスがセットされている() {
+		$ref = new ReflectionClass('Controller_Neapi');
+		$client = $ref->getProperty('client');
+		$client->setAccessible(true);
+
+		$this->assertInstanceOf('\NextEngine\Api\Client', $client->getValue());
+	}
 }
