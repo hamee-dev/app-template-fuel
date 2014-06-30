5cb4b31f7a21ea14380902e8225e9910d7aaf4f7
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-30 15:22:48 +0900

    Nextengine\Api\Clientのテストを記述、コメントを補足し無駄な処理を削除

--- fuel/app/tests/nextengine/api/client.php
+++ fuel/app/tests/nextengine/api/client.php
@@ -5,5 +5,93 @@
  */
 class Test_Nextengine_Api_Client extends PHPUnit_Framework_TestCase
 {
+	private function getMethod($class, $method) {
+		$ref = new ReflectionClass($class);
+
+		$method = $ref->getMethod($method);
+		$method->setAccessible(true);
+
+		return $method;
+	}
+	private function getProperty($class, $prop) {
+		$ref = new ReflectionClass($class);
+
+		$prop = $ref->getProperty($prop);
+		$prop->setAccessible(true);
+
+		return $prop;
+	}
+
+	function test_init_が呼ばれるとnextengineの設定が使用できる() {
+		Nextengine\Api\Client::_init();
+
+		$this->assertTrue(is_array(Config::get('nextengine')));
+	}
+
+	function test_forge_戻り値はNextengine_Api_Clientのインスタンス() {
+		$instance = Nextengine\Api\Client::forge();
+		$this->assertInstanceOf('Nextengine\Api\Client', $instance);
+	}
+
+	function test_construct_引数にクライアントID、シークレット、リダイレクトURIを渡すとプロパティに反映されている() {
+		$arg = array(
+			'client_id'     => 'aaa',
+			'client_secret' => 'bbb',
+			'redirect_uri'  => 'ccc',
+		);
+
+		$ref = new ReflectionClass('Nextengine\Api\Client');
+		$client = $ref->newInstanceArgs(array($arg));
+
+		$_client_id     = $this->getProperty('Nextengine\Api\Client', '_client_id');
+		$_client_secret = $this->getProperty('Nextengine\Api\Client', '_client_secret');
+		$_redirect_uri  = $this->getProperty('Nextengine\Api\Client', '_redirect_uri');
+
+		$this->assertEquals('aaa', $_client_id->getValue($client));
+		$this->assertEquals('bbb', $_client_secret->getValue($client));
+		$this->assertEquals('ccc', $_redirect_uri->getValue($client));
+	}
+
+	function test_setUser_クライアントのアクセストークンと渡したユーザのアクセストークンが違ったら、クライアント側のアクセストークンで上書きする() {
+		$mockUser = $this->getMock('Model_User', array('save'));
+		$mockUser->expects($this->once())
+				->method('save');
+
+		$client = Nextengine\Api\Client::forge();
+		$client->_access_token = 'hoge';
+		$client->_refresh_token = 'foo';
+
+		$client->setUser($mockUser);
+		$this->assertEquals($client->_access_token, $mockUser->access_token);
+		$this->assertEquals($client->_refresh_token, $mockUser->refresh_token);
+	}
+	function test_setUser_まだAPIが呼ばれていない状態でアクセストークンのあるユーザが渡されたらクワイアンと側のアクセストークンを上書き() {
+		$mockUser = $this->getMock('Model_User', array('save'));
+		$mockUser->expects($this->never())
+				->method('save');
+
+		$mockUser->access_token  = 'aaa';
+		$mockUser->refresh_token = 'bbb';
+
+		$client = Nextengine\Api\Client::forge();
+		$client->setUser($mockUser);
+
+		$this->assertEquals($mockUser->access_token, $client->_access_token);
+		$this->assertEquals($mockUser->refresh_token, $client->_refresh_token);
+	}
+
+	// FIXME: 親クラスのapiExecuteを書き換えてレスポンスを上書きする方法が分からない
+	// function test_apiExecute_APIからのレスポンスのresultフィールドがsuccessじゃなかったら例外を投げる() {
+	// 	$dummy_response = array(
+	// 		'result' => 'error'
+	// 	);
+	// }
+
+	function test_failover_呼び出されるとそのコード、メッセージでNextengineApiExceptionを投げる() {
+		$failover = $this->getMethod('Nextengine\Api\Client', 'failover');
+		$this->setExpectedException('Nextengine\Api\NextengineApiException', 'hoge', '0123');
+
+		$failover->invokeArgs(Nextengine\Api\Client::forge(), array('0123', 'hoge'));
+	}
 
 }
--- fuel/packages/nextengine/classes/nextengine/api/client.php
+++ fuel/packages/nextengine/classes/nextengine/api/client.php
@@ -4,19 +4,35 @@ namespace Nextengine\Api;
 
 require_once __DIR__.DS.'..'.DS.'..'.DS.'neApiClient.php';
 
+/**
+ * ネクストエンジンAPIからのレスポンスによって発生する例外クラス
+ * 発生する例外やその処理については継承クラスとドキュメントを参照。
+ * ドキュメント：http://api.next-e.jp/message.php
+ * @see Nextengine\Api\Client_Router
+ * @see Nextengine\Api\Client_Batch
+ */
 class NextengineApiException extends \FuelException {
+	/**
+	 * @param string $message エラーメッセージ(APIからのエラーメッセージをそのまま投げる)
+	 * @param string $code    エラーコード(APIからのエラーコードをそのまま投げる)
+	 */
 	public function __construct($message, $code) {
+		// NOTE: 先にcodeプロパティを定義してしまえば、
+		//       親クラスのコンストラクタでcodeプロパティが上書きされることはない
 		$this->code = $code;
-		parent::__construct($message, 0);
+		parent::__construct($message);
 	}
 }
 
+/**
+ * ネクストエンジンAPIクライアント
+ * 
+ * 例外による処理の振り分けを実装しているのは継承クラスなので、そちらを参照。
+ * @see Nextengine\Api\Client_Router
+ * @see Nextengine\Api\Client_Batch
+ */
 class Client extends \neApiClient
 {
-	const RESULT_SUCCESS  = 'success';
-	const RESULT_ERROR    = 'error';
-	const RESULT_REDIRECT = 'redirect';
-
 	/**
 	 * デフォルトの設定値
 	 * @var array
@@ -30,7 +46,8 @@ class Client extends \neApiClient
 	protected $user = null;
 
 	/**
-	 * 設定をロードし格納しておく
+	 * 設定をロードし、_defaultsプロパティに格納しておく
+	 * @return void
 	 */
 	public static function _init()
 	{
@@ -53,6 +70,7 @@ class Client extends \neApiClient
 	/**
 	 * コンストラクタに与えられた設定でデフォルト設定を上書きし、接続に必要な情報を格納する
 	 * @param  array      $config 設定値（この値が優先される）
+	 * @return Nextengine\Api\Client
 	 */
 	public function __construct(array $config = array())
 	{
@@ -62,6 +80,38 @@ class Client extends \neApiClient
 			$config['client_id'],
 			$config['client_secret'],
 			$config['redirect_uri']);
+	}
+
+	/**
+	 * ネクストエンジンAPIを叩く
+	 * 
+	 * ### 親クラスからの拡張点
+	 * レスポンスに含まれるresultフィールドの値がsuccessでないなら、例外を投げるという処理を追加した
+	 *
+	 * @throws NextengineApiException
+	 */
+	public function apiExecute($path, $api_params = array(), $redirect_uri = NULL) {
+		$before_exec_access_token = $this->_access_token;
+		$response = parent::apiExecute($path, $api_params, $redirect_uri);
+
+		// NOTE: エラーの種類については→を参照：http://api.next-e.jp/message.php
+		//       エラー時の振る舞いについては、継承クラスのfailoverメソッドを参照。
+		if($response['result'] !== self::RESULT_SUCCESS) {
+			$this->failover($response['code'], $response['message']);
+		}
+
+		// APIを叩く前後でアクセストークンが変わっていたら、ユーザモデルを更新してDBに反映、セッションも更新する
+		// ただしユーザモデルが格納されていない場合もあるので、その場合はその処理を行わない
+		if(!is_null($this->user) && $before_exec_access_token !== $this->_access_token) {
+			$this->user->access_token  = $this->_access_token;
+			$this->user->refresh_token = $this->_refresh_token;
+			$this->user->save();
+
+			$user_key = Config::get('session.keys.ACCOUNT_USER');
+			Session::set($user_key, $this->user);
+		}
+
+		return $response;
 	}
 
 	/**
@@ -88,39 +138,11 @@ class Client extends \neApiClient
 	}
 
 	/**
-	 * ネクストエンジンAPIを叩く
-	 * 
-	 * ### 拡張点
-	 * 親クラスから拡張した処理は、
-	 * レスポンスに含まれるresultフィールドの値がsuccessでないなら、例外を投げるという処理を追加した
-	 *
-	 * @override
-	 */
-	public function apiExecute($path, $api_params = array(), $redirect_uri = NULL) {
-		$before_exec_access_token = $this->_access_token;
-		$response = parent::apiExecute($path, $api_params, $redirect_uri);
-
-		// TODO: resultがsuccessじゃなかったら、エラーコードによって例外を投げる
-		// エラーの種類については→を参照：http://api.next-e.jp/message.php
-		// エラー時の振る舞いについては、継承クラスのfailoverメソッドを参照。
-		if($response['result'] !== self::RESULT_SUCCESS) {
-			$this->failover($response['code'], $response['message']);
-		}
-
-		if(!is_null($this->user) && $before_exec_access_token !== $this->_access_token) {
-			$this->user->access_token  = $this->_access_token;
-			$this->user->refresh_token = $this->_refresh_token;
-			$this->user->save();
-
-			$user_key = Config::get('session.keys.ACCOUNT_USER');
-			Session::set($user_key, $this->user);
-		}
-
-		return $response;
-	}
-
-	/**
 	 * ネクストエンジンAPIからエラーが帰っていた場合の処理を行う
+	 * @param  string $code    NextengineApiExceptionに渡すエラーコード
+	 * @param  string $message NextengineApiExceptionに渡すエラーメッセージ
+	 * @return void
+	 * @throws NextengineApiException
 	 */
 	protected function failover($code, $message) {
 		// TODO: ログの出力/メール送信
