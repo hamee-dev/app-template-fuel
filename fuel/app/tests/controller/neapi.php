1cb2a04ec293fe6fa3c9b3833eff9d19b00a0b6d
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-06-30 16:45:15 +0900

    シードに企業とテスト用NEアカウントのデータを追加

--- fuel/app/tests/controller/neapi.php
+++ fuel/app/tests/controller/neapi.php
@@ -11,11 +11,7 @@ class Test_Controller_Neapi extends Test_Controller_Base
 		parent::setUp();
 
 		// CLIからだとセッションが何も設定されてないので明示的にセット
-		// FIXME: ハードコードではなく、ユーザ情報のシードをテスト時に投入してそれを利用したい
-		$user = (object)array(
-			'id'  => 20,
-			'uid' => '71c1cc54f91641828805369f1948d22d8389cfefc6ca2661662aa4b5c8f15a61132dddd97710433b13ed9be9656630f040a5e9782f4847471031ecc02cb9d628'
-		);
+		$user = Model_User::find(1);
 
 		$user_key = Config::get('session.keys.ACCOUNT_USER');
 		Session::set($user_key, $user);
