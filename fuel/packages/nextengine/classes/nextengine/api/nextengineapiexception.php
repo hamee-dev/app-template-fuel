f12238fe053b83d37e1756035bd5d5149e48b9d1
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-08-18 17:02:02 +0900

    fuel/packages/nextengine/classes/nextengine/api/nextengineapiexception.phpの引数コメントを修正

--- fuel/packages/nextengine/classes/nextengine/api/nextengineapiexception.php
+++ fuel/packages/nextengine/classes/nextengine/api/nextengineapiexception.php
@@ -6,11 +6,14 @@ namespace Nextengine\Api;
  * ネクストエンジンAPIからのレスポンスによって発生する例外クラス
  * 発生する例外やその処理については継承クラスとドキュメントを参照。
  * ドキュメント：http://api.next-e.jp/message.php
+ * 
  * @see Nextengine\Api\Client_Router
  * @see Nextengine\Api\Client_Batch
  */
 class NextengineApiException extends \FuelException {
 	/**
+	 * コンストラクタ
+	 * 
 	 * @param string $message エラーメッセージ(APIからのエラーメッセージをそのまま投げる)
 	 * @param string $code    エラーコード(APIからのエラーコードをそのまま投げる)
 	 */
