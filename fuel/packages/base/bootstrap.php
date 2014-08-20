6e5dd6cd3c320e48c50cb24ddd95622663162113
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-08-20 14:35:35 +0900

    fuel/appにあったコントローラの基底クラスをbaseパッケージへ移動

--- fuel/packages/base/bootstrap.php
+++ fuel/packages/base/bootstrap.php
@@ -1,8 +1,11 @@
 <?php
 
-Autoloader::add_core_namespace('Base');
+Autoloader::add_namespace('Base', __DIR__.'/classes/');
 
 Autoloader::add_classes(array(
-	'Base\\Logger' => __DIR__ . '/classes/logger.php',
+	'Base\\Controller_Base' => __DIR__ . '/classes/controller/base.php',
+	'Base\\Controller_Auth' => __DIR__ . '/classes/controller/auth.php',
+
+	'Base\\Logger'          => __DIR__ . '/classes/logger.php',
 
 ));
---
+++ fuel/packages/base/classes/controller/base.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @author Shingo Inoue<inoue.shingo@hamee.co.jp>
+ */
+
+namespace Base;
+
+/**
+ * コントローラの基底クラス。
+ * 全画面で共通処理として必要な言語ファイルのロード、表示言語のセットを行う。
+ * 
+ * NOTE: Controllerとコアと同名のクラスにするとこちらが先にロードされてしまうのでControllerという命名を避けている
+ */
+abstract class Controller_Base extends \Controller_Template
+{
+	/**
+	 * アクセスされたユーザの言語ロケールを取得する
+	 * 
+	 * 複数指定されている場合は、もっとも先頭に指定されている言語を返却します。  
+	 * 先頭の２文字だけを見るので、３文字以上の言語略称が現れた場合にはバグります。直して下さい。
+	 * 
+	 * ### example
+	 * - 'en' => 'en'
+	 * - 'ja;q=0.6' => 'ja'
+	 * - 'xxx' => 'xx'
+	 * 
+	 * @param string $raw_locale ブラウザから送信された言語情報を表す文字列
+	 * @return string
+	 */
+	private static function getLocale($raw_locale) {
+		$locales = explode(',', $raw_locale);
+		return \Str::sub(trim($locales[0]), 0, 2);
+	}
+
+	/**
+	 * 言語の設定、言語ファイルの読み込みを行う
+	 * 
+	 * ここに書いておくことで継承クラスは言語ファイルのロードを考慮せずコーディングできる
+	 * このクラスを継承したクラスで_initを定義したい場合には、"必ず"parent::_init()をコールして下さい
+	 * 
+	 * @return void
+	 */
+	public static function _init() {
+		// 言語のセット
+		$locale = self::getLocale($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+		\Config::set('language', $locale);
+
+		// 言語ファイルのロード
+		\Lang::load('common.yml', 'common');
+		\Lang::load('model.yml', 'model');
+		\Lang::load('page.yml', 'page');
+		\Lang::load('message.yml', 'message');
+	}
+}
