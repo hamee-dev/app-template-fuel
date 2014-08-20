64105dab574d0456115f4cd0a18e2c5c85903590
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-08-20 14:22:57 +0900

    アプリの設定に合わせて設定を戻した

--- fuel/app/config/nextengine.php
+++ fuel/app/config/nextengine.php
@@ -3,7 +3,7 @@
 return array(
 	'client_id' => '2zG7d5MjXPh4m8',
 	'client_secret' => 'FTNubmlpyAgE5e3BnqWt6IHJ18voxVkMS9Yh4Zjc',
-	'redirect_uri' => 'https://192.168.33.15/auth/callback',
+	'redirect_uri' => 'https://192.168.33.10/auth/callback',
 
 	'debug' => array(
 		// デバッグや通知メールを送るべき開発者
