5ca3aca666c50b75051d432f0f6267ef38db89cf
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-09-11 15:51:51 +0900

    usersとcompaniesのカラムの並び順を変更、コードが横長になっていたので改行を入れた

--- fuel/packages/base/migrations/001_create_companies.php
+++ fuel/packages/base/migrations/001_create_companies.php
@@ -7,11 +7,28 @@ class Create_companies
 	public function up()
 	{
 		\DBUtil::create_table('companies', array(
-			'id' 				=> array('constraint' => 11,  'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-			'main_function_id' 	=> array('constraint' => 128, 'type' => 'char'),
-			'platform_id' 		=> array('constraint' => 128, 'type' => 'char'),
-			'updated_at' 		=> array('type' => 'timestamp', 'default' => \DB::expr('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
-			'created_at' 		=> array('type' => 'timestamp', 'default' => \DB::expr('CURRENT_TIMESTAMP')),
+			'id' => array(
+				'constraint' => 11,
+				'type' => 'int',
+				'auto_increment' => true,
+				'unsigned' => true
+			),
+			'main_function_id' => array(
+				'constraint' => 128,
+				'type' => 'char'
+			),
+			'platform_id' => array(
+				'constraint' => 128,
+				'type' => 'char'
+			),
+			'created_at' => array(
+				'type' => 'timestamp',
+				'default' => \DB::expr('CURRENT_TIMESTAMP')
+			),
+			'updated_at' => array(
+				'type' => 'timestamp',
+				'default' => \DB::expr('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+			),
 
 		), array('id'), true, 'InnoDB');
 
--- fuel/packages/base/migrations/002_create_users.php
+++ fuel/packages/base/migrations/002_create_users.php
@@ -7,15 +7,47 @@ class Create_users
 	public function up()
 	{
 		\DBUtil::create_table('users', array(
-			'id' 				=> array('constraint' => 11,  'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
-			'company_id' 		=> array('constraint' => 11,  'type' => 'int', 'unsigned' => true),
-			'uid' 				=> array('constraint' => 128, 'type' => 'char'),
-			'next_engine_id' 	=> array('constraint' => 128, 'type' => 'char'),
-			'email' 			=> array('constraint' => 255, 'type' => 'varchar'),
-			'access_token' 		=> array('constraint' => 128, 'type' => 'char', 'null' => true),
-			'refresh_token' 	=> array('constraint' => 128, 'type' => 'char', 'null' => true),
-			'updated_at' 		=> array('type' => 'timestamp', 'default' => \DB::expr('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
-			'created_at' 		=> array('type' => 'timestamp', 'default' => \DB::expr('CURRENT_TIMESTAMP')),
+			'id' => array(
+				'constraint' => 11,
+				'type' => 'int',
+				'auto_increment' => true,
+				'unsigned' => true
+			),
+			'company_id' => array(
+				'constraint' => 11,
+				'type' => 'int',
+				'unsigned' => true
+			),
+			'uid' => array(
+				'constraint' => 128,
+				'type' => 'char'
+			),
+			'next_engine_id' => array(
+				'constraint' => 128,
+				'type' => 'char'
+			),
+			'email' => array(
+				'constraint' => 255,
+				'type' => 'varchar'
+			),
+			'access_token' => array(
+				'constraint' => 128,
+				'type' => 'char',
+				'null' => true
+			),
+			'refresh_token' => array(
+				'constraint' => 128,
+				'type' => 'char',
+				'null' => true
+			),
+			'created_at' => array(
+				'type' => 'timestamp',
+				'default' => \DB::expr('CURRENT_TIMESTAMP')
+			),
+			'updated_at' => array(
+				'type' => 'timestamp',
+				'default' => \DB::expr('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+			),
 
 		), array('id'), true, 'InnoDB');
 
