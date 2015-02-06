24f26681633ddb3574059ee1d92414cd3b590666
Shingo Inoue <inoue.shingo@hamee.co.jp>
2015-02-06 17:35:09 +0900

    packages/base/classes/controller/errors.phpのエラーを修正

--- fuel/packages/base/classes/controller/errors.php
+++ fuel/packages/base/classes/controller/errors.php
@@ -10,20 +10,36 @@ namespace Base;
  */
 abstract class Controller_Errors extends Controller_Base
 {
+	/**
+	 * このコントローラ配下の画面で使用するテンプレートファイル名(.phpは除く)
+	 * @var string
+	 */
 	public $template = 'template-error';
 
+	/**
+	 * エラー画面を描画する
+	 * @return void
+	 */
 	public function get_index()
 	{
 		$this->template->title = 'Error &raquo; Index';
 		$this->template->content = \View::forge('errors/index');
 	}
 
+	/**
+	 * メンテナンス中のエラー画面を描画する
+	 * @return void
+	 */
 	public function get_maintenance()
 	{
 		$this->template->title = 'Error &raquo; Maintenance';
 		$this->template->content = \View::forge('errors/maintenance');
 	}
 
+	/**
+	 * システムが混み合っている場合のエラー画面を描画する
+	 * @return void
+	 */
 	public function get_congestion()
 	{
 		$this->template->title = 'Error &raquo; Congestion';
