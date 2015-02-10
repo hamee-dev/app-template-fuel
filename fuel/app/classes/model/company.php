8ec9ac5070d873e5d5856e4399f29c4528afce30
hamee-dev <development@hamee.co.jp>
2015-02-10 12:38:26 +0900

    Model_Company, Model_Userのクラス概要コメントを修正

--- fuel/app/classes/model/company.php
+++ fuel/app/classes/model/company.php
@@ -1,9 +1,6 @@
 <?php
 
 /**
- * 企業情報を保持するモデル
- * 
- * Base\Model_Companyは改善やバグ修正などで随時変更されます。
- * 差分を取り込む際のコストを低減するために、各アプリごとの拡張はこのクラスに実装して下さい。
+ * ネクストエンジンの企業情報を保持するモデル
  */
 class Model_Company extends \Base\Model_Company {}
